c1411b2bd6633567743a2383f3a17de9137adce3
Andrea DeMers <andreademers@gmail.com>
2013-09-08 11:59:05 -0400

    Add caching

--- twitter_embed/pi.twitter_embed.php
+++ twitter_embed/pi.twitter_embed.php
@@ -23,21 +23,29 @@ class Plugin_twitter_embed extends Plugin {
 
     // Make sure tweet id is present
     if (isset($params['id'])) {
-
       // Twitter API
       $resource_url = 'https://api.twitter.com/1/statuses/oembed.json?';
 
       // Build request url
       $request_url = $resource_url . http_build_query($params);
 
-      // Get json request as string
-      $contents = file_get_contents($request_url);
+      // Hash request url for caching
+      $file_name = md5($request_url);
 
-      // Decode json string
-      $response = json_decode($contents);
+      // If not already cached or if cache 2 weeks or older, cache it
+      if (!$this->cache->exists($file_name) || $this->cache->getAge($file_name) >= 1209600) {
+        // Get json request as string
+        $contents = file_get_contents($request_url);
 
-      // Output html from json string
-      return $response->html;
+        // Decode json string
+        $response = json_decode($contents);
+
+        // Store html from json string in cache
+        $this->cache->put($file_name, $response->html);
+      }
+
+      // Output html from json string from cache
+      return $this->cache->get($file_name);
     }
   }
 }
